7f28673f3825f7cb050f8b0a35b816eaefce4aab
Yaroslav <Yaroslav.Kuzmin.01@yandex.ru>
2023-05-04 00:48:07 +0300

    Добавил окна и крыльцо

--- lw/lw5/1/script.js
+++ lw/lw5/1/script.js
@@ -63,6 +63,20 @@ function getFloorTexture() {
     return texture
 }
 
+function getFrameTexture() {
+    const textureLoader = new THREE.TextureLoader()
+    return textureLoader.load('./assets/frame.jpg')
+}
+
+function getBushTexture() {
+    const textureLoader = new THREE.TextureLoader()
+    const texture = textureLoader.load('./assets/bush.jpg')
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+    texture.repeat.set(5, 5)
+    return texture
+}
+
 function getVerandaMaterialSettings() {
     return {
         color: '#D5713F',
@@ -202,6 +216,245 @@ function createGates() {
     return door
 }
 
+function createWindow1() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(0, 1.25, 2)
+    return window
+}
+
+function createWindow2() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(1, 1.25, 2)
+    return window
+}
+
+function createWindow3() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(1, 2.5, 2)
+    return window
+}
+
+function createWindow4() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(0, 2.5, 2)
+    return window
+}
+
+function createWindow5() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-1, 2.5, 2)
+    return window
+}
+
+function createWindow6() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(1 / 16, 0.5, 0.5),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-2, 2.5, 1)
+    return window
+}
+
+function createWindow7() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(1 / 16, 0.5, 0.5),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-2, 2.5, 0)
+    return window
+}
+
+function createWindow8() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(1 / 16, 0.5, 0.5),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-2, 2.5, -1)
+    return window
+}
+
+function createWindow9() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(1 / 16, 0.5, 0.5),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-2, 1.25, 1)
+    return window
+}
+
+function createWindow10() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(1 / 16, 0.5, 0.5),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-2, 1.25, 0)
+    return window
+}
+
+function createWindow11() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(1 / 16, 0.5, 0.5),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-2, 1.25, -1)
+    return window
+}
+
+function createWindow12() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(1, 1.25, -2)
+    return window
+}
+
+function createWindow13() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(0, 1.25, -2)
+    return window
+}
+
+function createWindow14() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-1, 1.25, -2)
+    return window
+}
+
+function createWindow15() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(1, 2.5, -2)
+    return window
+}
+
+function createWindow16() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(-1, 2.5, -2)
+    return window
+}
+
+function createWindow17() {
+    const window = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 1 / 16),
+        new THREE.MeshPhongMaterial({
+            map: getFrameTexture(),
+        })
+    )
+    window.position.set(0, 2.5, -2)
+    return window
+}
+
+function createWindows() {
+    return [
+        createWindow1(),
+        createWindow2(),
+        createWindow3(),
+        createWindow4(),
+        createWindow5(),
+        createWindow6(),
+        createWindow7(),
+        createWindow8(),
+        createWindow9(),
+        createWindow10(),
+        createWindow11(),
+        createWindow12(),
+        createWindow13(),
+        createWindow14(),
+        createWindow15(),
+        createWindow16(),
+        createWindow17(),
+    ]
+}
+
+function createPorch() {
+    const porch = new THREE.Object3D
+    const roof = new THREE.Mesh(
+        new THREE.BoxGeometry(0.8, 1 / 16, 0.8),
+        new THREE.MeshPhongMaterial({
+            color: '#36170a',
+        })
+    )
+    roof.position.set(-1, 1.1, 2.2)
+    const leftSupport = new THREE.Mesh(
+        new THREE.CylinderGeometry( 0.05, 0.05, 1.1, 16 ),
+        new THREE.MeshBasicMaterial({
+            color: '#36170a',
+        })
+    )
+    leftSupport.position.set(-1.35, 0.55, 2.55)
+    const rightSupport = new THREE.Mesh(
+        new THREE.CylinderGeometry( 0.05, 0.05, 1.1, 16 ),
+        new THREE.MeshBasicMaterial({
+            color: '#36170a',
+        })
+    )
+    rightSupport.position.set(-0.65, 0.55, 2.55)
+
+    porch.add(roof)
+    porch.add(leftSupport)
+    porch.add(rightSupport)
+    return porch
+}
+
 function createHouse() {
     const house = new THREE.Group()
 
@@ -211,12 +464,16 @@ function createHouse() {
     const mainDoor = createMainDoor()
     const verandaDoor = createVerandaDoor()
     const gates = createGates()
+    const windows = createWindows()
+    const porch = createPorch()
     house.add( ...walls )
     house.add( ...roofs )
     house.add( ...veranda )
+    house.add( ...windows )
     house.add( mainDoor )
     house.add( verandaDoor )
     house.add( gates )
+    house.add( porch )
 
     return house
 }
@@ -234,10 +491,33 @@ function createGround() {
     return ground
 }
 
+function createBushMesh(r, x, y, z) {
+    const bush = new THREE.Mesh(
+        new THREE.SphereGeometry(r, 32, 16),
+        new THREE.MeshBasicMaterial({
+            map: getBushTexture(),
+        })
+    )
+    bush.position.set(x, y, z)
+    return bush
+}
+
+function createBush() {
+    const bush = new THREE.Group()
+    bush.add(createBushMesh(0.35, 0.25, 0.2, 2.3))
+    bush.add(createBushMesh(0.3, 0.8, 0.2, 2.3))
+    bush.add(createBushMesh(0.35, 1.8, 0.2, 2.3))
+    bush.add(createBushMesh(0.35, 5.4, 0.2, 1.5))
+    bush.add(createBushMesh(0.35, 5.4, 0.2, 0.5))
+    bush.add(createBushMesh(0.35, 5.4, 0.2, -0.5))
+    return bush
+}
+
 function createScene() {
     const scene = new THREE.Scene()
     scene.add(createGround())
     scene.add(createHouse())
+    scene.add(createBush())
     return scene
 }
 
@@ -305,7 +585,7 @@ function init() {
     )
     const renderer = new THREE.WebGLRenderer()
     scene.background = getSkyTexture()
-    camera.position.set( 0, 10, 250 )
+    camera.position.set( 0, 15, 250 )
     renderer.setSize( window.innerWidth, window.innerHeight )
     document.body.appendChild( renderer.domElement )
 
